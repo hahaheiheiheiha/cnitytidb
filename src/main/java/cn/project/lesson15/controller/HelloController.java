5d0f23828af3b63f8c42bdb1288f4f1c66086957
2891792857 <2891792857@qq.com>
2020-08-22 13:50:06 +0800

    kkk

---
+++ src/main/java/cn/project/lesson15/controller/HelloController.java
@@ -0,0 +1,13 @@
+package cn.project.lesson15.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello word";
+    }
+}
---
+++ src/test/java/cn/project/lesson15/Lesson15ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.project.lesson15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson15ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
